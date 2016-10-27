294ea4dc9f09740202dd923c0322919b899d7cc9
Rishabh <rishabhjindal4@gmail.com>
2016-10-28 00:04:43 +0530

    file complete with 10 exp

--- src/javalab/Exp10A.java
+++ src/javalab/Exp10A.java
@@ -13,32 +13,27 @@ import java.util.logging.Logger;
  * @author rishabh
  */
 public class Exp10A extends Thread{
-
-	public void run()
-	{
-		System.out.println(Thread.currentThread().getName()+": "+Thread.currentThread().getPriority());
-		try {
-        		Thread.sleep(1000);
-    		}
-    		catch(InterruptedException ie) { }
-       		System.out.println("r2 ");
-  	}
-	public static void main(String[] args)
-	{
-		Exp10A t1=new Exp10A();
-		Exp10A t2=new Exp10A();
-		
-                t1.setPriority(9);
-                t2.setPriority(1);
-                t1.start(); 
-		t2.start();
-            try {
-                t1.join();
-                
-            } catch (InterruptedException ex) {
-               
-            }
-		System.out.println(t1.isAlive());
-		System.out.println(t2.isAlive());
-	}
+    public void run()
+    {
+        System.out.println(Thread.currentThread().getName()+": "+Thread.currentThread().getPriority());
+        try {
+            Thread.sleep(1000);
+        }
+        catch(InterruptedException ie) { }
+        System.out.println("r2 ");
+    }
+    public static void main(String[] args)	{
+        Exp10A t1=new Exp10A();
+        Exp10A t2=new Exp10A();
+        t1.setPriority(9);
+        t2.setPriority(1);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+        } catch (InterruptedException ex) {
+        }
+        System.out.println(t1.isAlive());
+        System.out.println(t2.isAlive());
+    }
 }
